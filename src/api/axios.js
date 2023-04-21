fdc97372f519a5efd78b2664457344f400332129
ohchanghoon <dhckdgns3@naver.com>
2023-04-21 13:58:01 +0900

    add: 전체 구조 생성

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+const { default: axios } = require("axios");
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: "f438a92ea58a51063a5de7e1a2b3e998",
+        language: "ko-KR"
+    }
+});
+
+export default instance;
