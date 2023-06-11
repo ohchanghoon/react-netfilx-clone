181bc6b5d25f31bf84b7f6acfe43748dd856cbae
ohchanghoon <dhckdgns3@naver.com>
2023-06-11 15:09:43 +0900

    add: play button 클릭 시 비디오로 전환

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 import requests from '../api/requests';
 import axios from '../api/axios';
 import "./Banner.css"
+import { styled } from 'styled-components';
 
 function truncate(str, n) {
-    return str?.length > n ? str.substr(0, n - 1) + "..." : str 
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str
 }
 
 export default function Banner() {
-    const [movie, setmovie] = useState([])
+    const [movie, setmovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
+
     useEffect(() => {
         fetchData();
     }, [])
@@ -29,23 +32,75 @@ export default function Banner() {
         setmovie(movieDetail)
     }
 
-    return <header
-       className="banner"
-        style={{
-          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-          backgroundPosition: "top center",
-          backgroundSize: "cover",
-        }}
-    >
-        <div className='banner__contents'>
-            <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
-            <div className='banner__buttons'>
-                <button className='banner__button play'>Play</button>
-                <button className='banner__button info'>More Information</button>
-            </div>
+    if (!isClicked) {
+        return <header
+            className="banner"
+            style={{
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                backgroundPosition: "top center",
+                backgroundSize: "cover",
+            }}
+        >
+            <div className='banner__contents'>
+                <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button play' onClick={() => setIsClicked(true)}>Play</button>
+                    <button className='banner__button info'>More Information</button>
+                </div>
 
-            <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
-        </div>
-        <div className='banner--fadeBottom' />
-    </header>
+                <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
+            </div>
+            <div className='banner--fadeBottom' />
+        </header>
+    } else {
+        return (
+            <Container>
+                <HomeContainer>
+                    {movie.videos.results.length > 0 ?
+                        <Iframe
+                            width="640"
+                            height="360"
+                            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playist=${movie.videos.results[0].key}`}
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="autoplay; fullscreen"
+                            allowfullscreen>
+                        </Iframe>
+                        :
+                        'No Videos'
+                    }
+                </HomeContainer>
+            </Container>
+        )
+    }
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    border: none;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`
